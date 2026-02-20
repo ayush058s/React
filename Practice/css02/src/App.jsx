a4367a192cf7067c6f5ec1eb78f6e7fdc5c0b08f
ayush058s <ayush05singh08@gmail.com>
2026-02-20 17:12:49 +0530

    module css practice

---
+++ Practice/css02/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/header'
+import Button from './components/button/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ Practice/css02/src/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import csss from'./buttton.module.css'
+
+const Button = () => {
+  return (
+    <div className={csss.btn}>button</div>
+  )
+}
+
+export default Button
