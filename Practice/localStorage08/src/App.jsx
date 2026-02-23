aeb11bf22333d22e26b2880757839ca767cb1b28
ayush058s <ayush05singh08@gmail.com>
2026-02-23 20:16:54 +0530

    local and session storage

---
+++ Practice/localStorage08/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  localStorage.clear(); // store data on browser, stored in key value pair, AND IN STRING
+  sessionStorage.clear(); // data is stored for a session if browser is closed it will be gone
+
+  localStorage.setItem('user', 'Ayush');  
+
+  const user = localStorage.getItem('user');
+  console.log(user);
+
+  const user1 = {
+    name: 'Ayush',
+    age: 29,
+    city: 'Bhopal'
+  }
+
+  localStorage.setItem('user1', JSON.stringify(user1));
+  const userFetched = JSON.parse(localStorage.getItem('user1'));
+  console.log(userFetched);
+
+  return (
+    <div >
+      <h1 className='text5xl'>local and session storage</h1>
+    </div>
+  )
+}
+
+export default App
